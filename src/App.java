2abbc33f6fde14915e84661a001492985afa156d
dev-davimarques <davimarques10.dm@icloud.com>
2025-10-05 12:44:59 -0300

    fix

--- src/App.java
+++ src/App.java
@@ -1,14 +1,10 @@
 import java.util.Locale;
 
-import utils.ContaBancaria;
 import utils.ContaPoupanca;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
-        // ContaBancaria c1 = new ContaBancaria("Davi", 100, 1000);
-        // System.out.println(c1.toString());
-        // c1.sacar(1100);
 
         ContaPoupanca c2 = new ContaPoupanca("Cliente 2", 300, 100, 4);
         // Valor 1
