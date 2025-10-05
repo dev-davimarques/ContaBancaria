0f415a77ffc5aab60810d43e0560587e3d02832c
dev-davimarques <davimarques10.dm@icloud.com>
2025-10-05 14:55:25 -0300

    Conta corrente

--- src/App.java
+++ src/App.java
@@ -1,22 +1,32 @@
 import java.util.Locale;
 
+import utils.ContaCorrente;
 import utils.ContaPoupanca;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
 
-        ContaPoupanca c2 = new ContaPoupanca("Cliente 2", 300, 100, 4);
-        // Valor 1
-        System.out.println(c2.toString());
-        c2.calcularNovoSaldo();
-        System.out.println("Saldo atual: "+c2.getSaldo());
+        // ContaPoupanca c2 = new ContaPoupanca("Cliente 2", 300, 100, 4);
+        // // Valor 1
+        // System.out.println(c2.toString());
+        // c2.calcularNovoSaldo();
+        // System.out.println("Saldo atual: "+c2.getSaldo());
         
-        System.out.println("----");
+        // System.out.println("----");
 
-        c2.setTaxaRendimento(6);
-        System.out.println(c2.toString());
-        c2.calcularNovoSaldo();
-        System.out.printf("Saldo atual: %.2f%n",c2.getSaldo());
+        // c2.setTaxaRendimento(6);
+        // System.out.println(c2.toString());
+        // c2.calcularNovoSaldo();
+        // System.out.printf("Saldo atual: %.2f%n",c2.getSaldo());
+
+        ContaCorrente c3 = new ContaCorrente("Davi", 454, 100, 100);
+        System.out.println(c3.toString());
+        c3.sacar(100);
+        System.out.println(c3.toString());
+        c3.sacar(100);
+        System.out.println(c3.toString());
+        c3.sacar(100);
+        System.out.println(c3.toString());
     }
 }
---
+++ src/utils/ContaCorrente.java
@@ -0,0 +1,42 @@
+package utils;
+
+public class ContaCorrente extends ContaBancaria{
+    private double limite;
+
+    // Construtor
+    public ContaCorrente(String cliente, int numConta, double saldo, double limite){
+        super(cliente, numConta, saldo);
+        if (limite < 0){
+            throw new IllegalArgumentException("O limite não pode ser negativo.");
+        }
+        this.limite = limite;
+    }
+
+    // Getters e Setters
+    public double getLimite() {
+        return limite;
+    }
+    public void setLimite(double limite) {
+        if (limite < 0){
+            throw new IllegalArgumentException("O limite não pode ser negativo.");
+        }
+        this.limite = limite;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (valor <= 0){
+            throw new IllegalArgumentException("Valor de depósito deve ser positivo.");
+        }
+        if (valor > (saldo + limite)) {
+            throw new IllegalArgumentException("Limite insuficiente");
+        } else {
+            saldo-=valor;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Cliente: %s | Conta: %d | Saldo Inicial: %.2f | Limite: %.2f",getCliente(), getNumConta(), saldo, limite);
+    }
+}
